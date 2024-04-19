128fbde283932444476d62743e4e5169d6c741a0
athul-08 <athulprasad0812@gmail.com>
2024-04-19 22:50:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,47 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task';
+
+  ngOnInit(): void {
+    this.showCustomAlert();
+  }
+
+  async showCustomAlert() {
+    try {
+      const result = await Swal.fire({
+        title: 'Custom UI Example',
+        html: `
+          <div style="text-align: center;">
+            <h3>This is a custom SweetAlert2 modal!</h3>
+            <p>You can add any HTML content here.</p>
+            <button id="custom-button" class="btn">Custom Button</button>
+          </div>
+        `,
+        showCloseButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel',
+        showLoaderOnConfirm: true,
+        preConfirm: () => {
+          // Perform actions when the Confirm button is clicked
+          return new Promise<void>((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 2000);
+          });
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+      });
+
+      if (result.isConfirmed) {
+        Swal.fire('Confirmed', 'Your action has been confirmed.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your action has been cancelled.', 'error');
+      }
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
+  }
 }
